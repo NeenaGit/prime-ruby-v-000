8be21eb14b17ee8a331d847e6aec3d8865f52927
Neena van de Water <nkuriakose@hotmail.com>
2017-11-15 18:37:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
-def prime(integer)
-
+def prime?(integer)
+   integer.select do|prime|
+     Prime_number = false
+     x = 2
+     if (prime % x == 0)
+       Prime_number = false
+    else
+      Prime_number = true      
+   end
 end# Add  code here!

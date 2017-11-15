c866b0c8cd4a24a69e47ff6bdff9a0a7e549d913
Neena van de Water <nkuriakose@hotmail.com>
2017-11-15 19:38:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(integer)
-   (2..integer).to_a.none? do|prime|
-   (prime % integer == 0)
+  if integer <= 1
+     false
+  else      
+    (2..integer).to_a.none? do|prime|
+    (prime % integer == 0)
+    end
    end
 end
